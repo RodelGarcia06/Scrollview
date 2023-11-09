91f00f302409d04739436c41b0b594f548ea860e
RodelGarcia06 <143071000+RodelGarcia06@users.noreply.github.com>
2023-11-10 06:37:34 +0800

    Scrollview

---
+++ src/Guest.js
@@ -0,0 +1,82 @@
+import react from "react";
+
+export const guest = [
+    {
+        id: 1,
+        name: "FELISILDA, ARNOLD JAMES",
+        place: "Singapore"
+    },
+    {
+        id: 2,
+        name: "PAGUICAN, NICOLE JAMES",
+        place: "Canada, Ottawa"
+    },
+    {
+        id: 3,
+        name: "GARCIA, RODEL",
+        place: "Phillipines, Bohol"
+    },
+    {
+        id: 3,
+        name: "TAHIL, ADRIAN JANE",
+        place: "Thailand, Bangkok"
+    },
+    {
+        id: 3,
+        name: "JAGUNOS, JOANA",
+        place: "United States of America"
+    },
+    {
+        id: 3,
+        name: "KIM SOO-HYUN",
+        place: "South Korea, Seoul"
+    },
+    {
+        id: 3,
+        name: "CARBONILLA, MARLON",
+        place: "Japan, Hanopol"
+    },
+    {
+        id: 3,
+        name: "SEO YEA-JI",
+        place: "South Korea, Seoul"
+    },
+    {
+        id: 3,
+        name: "ZAIJAN JARANILLA",
+        place: "Philippines, Oriental Mindoro"
+    },
+    {
+        id: 3,
+        name: "MACKENYU ARATA",
+        place: "Japan, Tokyo"
+    },
+    {
+        id: 3,
+        name: "PARK HYUNG-SIK",
+        place: "South Korea, Daegu"
+    },
+    {
+        id: 3,
+        name: "JILLIAN WARD",
+        place: "Philippines, Manila"
+    },
+    {
+        id: 3,
+        name: "JUSTIN TIMBERLAKE",
+        place: "Los Angeles, Shelby Forest"
+    },
+    {
+        id: 3,
+        name: "SARABIA, EARL MIKE",
+        place: "Japan, Tokyo"
+    },
+    {
+        id: 3,
+        name: "ANNA KENDRICK",
+        place: "Portland"
+    },
+
+]
+
+export default guest;
